021c9e11603e6d04b12ed03adfb436f01ad9089f
unn4m3d <smelnikov871@gmail.com>
2018-02-22 18:02:51 +1000

    Keep prefix if unprefixed name starts with digit

--- src/crystal_lib/prefix_matcher.cr
+++ src/crystal_lib/prefix_matcher.cr
@@ -5,6 +5,7 @@ class CrystalLib::PrefixMatcher
   def match(name : String)
     @prefixes.each do |prefix|
       if name.starts_with?(prefix)
+        next if name[prefix.size..-1].match(/^\d/)
         name = name[prefix.size..-1] if @remove_prefix
         return name
       end
